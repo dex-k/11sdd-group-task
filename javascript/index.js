0fa3cde3480ac78a3e4c35d878096615630409e4
dex <dex-k@users.noreply.github.com>
2018-05-24 20:32:43 +1000

    starting to set up particles.js

--- javascript/index.js
+++ javascript/index.js
@@ -5,4 +5,9 @@
 $(document).ready(function() {
     console.log("Document ready");
 
+    //sets up particle background ([id of particles div], [path of json config], [callback function])
+    particlesJS.load('particles-js', 'particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+    
 });
