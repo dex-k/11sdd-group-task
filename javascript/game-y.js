ba76f4435d3a6941d57377439a58ee36633bc519
dex <dex-k@users.noreply.github.com>
2018-06-20 21:39:39 +1000

    removed old drag functions

--- javascript/game-y.js
+++ javascript/game-y.js
@@ -232,32 +232,6 @@ var tile = {
     e.originalEvent.dataTransfer.clearData();
   }
 }
-var allowDrop = function(e) {
-  e.preventDefault();
-}
-var drag = function(e) {
-    // console.log('start drag on tile ' + e.target.id)
-  e.originalEvent.dataTransfer.setData("text", e.target.id)
-  //NOT WORKING
-  setTimeout(()=>{
-    $('#' + e.target.id).css('visibility', 'hidden'); 
-  }, 1)
-}
-var drop = function(e) {
-    // console.log('droped onto ' + e.target.id);
-  var dragged = e.originalEvent.dataTransfer.getData("text");
-  var dropped = e.target.id;
-  console.log("(dragged " + dragged + ", onto " + dropped + ")");
-
-  //THE MAGIC HAPPENS HERE
-  /* NOT WORKING*/ $('#' + dragged).css('visibility', '');
-  swapTiles(dragged, dropped)
-}
-var dragEnd = function(e) {
-  // var dragged = e.originalEvent.dataTransfer.getData("text");
-  $('#' + e.target.id).css('visibility', '');
-  e.originalEvent.dataTransfer.clearData();
-}
 //Anything that changes the html/page here
 $(document).ready(function() {
   console.log("Document ready");
@@ -266,6 +240,7 @@ $(document).ready(function() {
   particlesJS('particles-js', particlesConfig);
   console.log('particles.js loaded')
   
+  //add drag event handlers
   $('.picture-tile').on("dragstart", tile.drag)
                     .on("dragover", tile.allowDrop)
                     .on("drop", tile.drop)
